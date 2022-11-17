21c767e9e7fd33ba920d15f790feb91b9273fe28
enessahin9 <enessahin9@outlook.com>
2022-11-17 19:09:00 +0300

    Practice has been done

---
+++ java101/daireAlanCevre/src/daireAlanCevre/Main.java
@@ -0,0 +1,23 @@
+package daireAlanCevre;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int r;
+		double pi = 3.14;
+
+		Scanner inp = new Scanner(System.in);
+
+		System.out.print("Dairenin yari capini giriniz : ");
+		r = inp.nextInt();
+		double alan = pi * r * r;
+		double cevre = 2 * pi * r;
+
+		System.out.println("Dairenin alani : " + alan);
+		System.out.println("Dairenin cevresi : " + cevre);
+
+	}
+
+}

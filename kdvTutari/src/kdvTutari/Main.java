ac0c7d69691abeddd145ec01058648c76872ab0c
enessahin9 <enessahin9@outlook.com>
2022-11-17 18:26:07 +0300

    Practice has been done

---
+++ kdvTutari/src/kdvTutari/Main.java
@@ -0,0 +1,20 @@
+package kdvTutari;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double tutar, kdvOran = 0.18, kdvTutar, kdvliTutar;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Ucret tutarini giriniz : ");
+		tutar = input.nextDouble();
+		kdvTutar = tutar * kdvOran;
+		kdvliTutar = tutar + kdvTutar;
+		System.out.println("KDV'siz Tutar : " + tutar);
+		System.out.println("KDV Orani : " + kdvOran);
+		System.out.println("KDV Tutari : " + kdvTutar);
+		System.out.println("KDV'li Tutar : " + kdvliTutar);
+	}
+
+}

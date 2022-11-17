716bedce75f2818dda50fd5c6285dffb4e3ec24d
enessahin9 <enessahin9@outlook.com>
2022-11-17 18:38:36 +0300

    Practice has been done

---
+++ java101/hipotenus/src/hipotenus/Main.java
@@ -0,0 +1,23 @@
+package hipotenus;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Degiskenleri olusturalim.
+		int a, b;
+		double c;
+
+		// Kullanicidan verileri alalim.
+		Scanner girdi = new Scanner(System.in);
+		System.out.print("1. Kenari giriniz : ");
+		a = girdi.nextInt();
+		System.out.print("2. Kenari giriniz : ");
+		b = girdi.nextInt();
+
+		c = Math.sqrt((a * a) + (b * b));
+		System.out.println("Hipotenus : " + c);
+	}
+
+}
